f80f887b6994592ed3d9eb9692c7010ff0a6797a
Evgeniy Palguev <dizverst@gmail.com>
2015-08-14 14:49:09 +0300

    создание БД

--- app.rb
+++ app.rb
@@ -13,7 +13,13 @@ before do
 end
 
 configure do
-
+	init_db 
+	@db.execute 'CREATE TABLE IF NOT EXISTS Posts
+	(
+	  id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	  created_date DATE NOT NULL,
+	  content TEXT NOT NULL
+	);'
 end
 
 get '/' do
