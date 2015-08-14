f077906d5073ba6d815b1b0039f7d8c089215f87
Evgeniy Palguev <dizverst@gmail.com>
2015-08-14 14:33:31 +0300

    некоторые изменения

--- app.rb
+++ app.rb
@@ -1,6 +1,20 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
+def init_db
+	@db = SQLite3::Database.new 'blog.sqlite'
+	@db.results_as_hash = true
+end
+
+before do
+	init_db
+end
+
+configure do
+
+end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
@@ -8,4 +22,10 @@ end
 
 get '/new' do
   erb :new
+end
+
+post '/new' do
+	content = params[:content]
+
+	erb "Вы написали #{content}"
 end
